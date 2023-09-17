d38c437d76dd48ff43c5d301442f8efbe524f2b9
balandina-o <balandina.olga.2021@gmail.com>
2023-09-17 14:52:43 +0500

    добавление метода fetch

--- src/App.js
+++ src/App.js
@@ -2,50 +2,54 @@ import AppendUser from "./components/AppendUser";
 import UserList from "./components/UserList";
 import React, { useState } from "react";
 
+import axios from "axios";
+
 function App() {
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      email: "dfdfddf",
-      first_name: "dfdf",
-      last_name: "dfdffd",
-      year: 28,
-      avatar:
-        "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
-    },
-    {
-      id: 2,
-      email: "dfdfddf",
-      first_name: "dfdf",
-      last_name: "dfdffd",
-      year: 28,
-      avatar:
-        "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
-    },
-    {
-      id: 3,
-      email: "dfdfddf",
-      first_name: "dfdf",
-      last_name: "dfdffd",
-      year: 28,
-      avatar:
-        "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
-    },
-    {
-      id: 4,
-      email: "dfdfddf",
-      first_name: "dfdf",
-      last_name: "dfdffd",
-      year: 28,
-      avatar:
-        "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
-    },
-  ]);
+  // const [users, setUsers] = useState([
+  //   {
+  //     id: 1,
+  //     email: "dfdfddf",
+  //     first_name: "dfdf",
+  //     last_name: "dfdffd",
+  //     year: 28,
+  //     avatar:
+  //       "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
+  //   },
+  //   {
+  //     id: 2,
+  //     email: "dfdfddf",
+  //     first_name: "dfdf",
+  //     last_name: "dfdffd",
+  //     year: 28,
+  //     avatar:
+  //       "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
+  //   },
+  //   {
+  //     id: 3,
+  //     email: "dfdfddf",
+  //     first_name: "dfdf",
+  //     last_name: "dfdffd",
+  //     year: 28,
+  //     avatar:
+  //       "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
+  //   },
+  //   {
+  //     id: 4,
+  //     email: "dfdfddf",
+  //     first_name: "dfdf",
+  //     last_name: "dfdffd",
+  //     year: 28,
+  //     avatar:
+  //       "https://i.pinimg.com/236x/d8/66/c0/d866c082e9338e9d0bd6786d9817fc64.jpg",
+  //   },
+  // ]);
 
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [year, setYear] = useState("");
+
+  const [users, setUsers] = useState([]);
 
   const appendUser = (newUser) => {
     setUsers([...users, newUser]);
@@ -55,9 +59,15 @@ function App() {
     setUsers(users.filter((user) => user.id !== userId));
   };
 
+  async function fetchUsers() {
+    const res = await axios.get("https://reqres.in/api/users?page=2");
+    console.log("users: ", res);
+  }
+
   return (
     <div>
       <AppendUser appendUser={appendUser} />
+      <button onClick={fetchUsers}>Load Users</button>
       <UserList userList={users} deleteUserItem={deleteUser} />;
     </div>
   );
